41bc7296d7ada0a21fe598581ee7527b2a07cc49
Heena <hk7780050@gmail.com>
2026-07-05 19:25:03 +0530

    script.js added

---
+++ script.js
@@ -0,0 +1,30 @@
+// find even or odd
+
+let firstNum=10;
+let secondNum=5;
+let thirdNum=4;
+let fourthNum=17;
+
+if(firstNum %2 ==0){
+    console.log("The num", firstNum , "is even");   
+} else{
+    console.log("The num", firstNum , "is odd");   
+}
+
+if(secondNum %2 ==0){
+    console.log("The num", secondNum , "is even");   
+} else{
+    console.log("The num", secondNum , "is odd");   
+}
+
+if(thirdNum %2 ==0){
+    console.log("The num", thirdNum , "is even");   
+} else{
+    console.log("The num", thirdNum , "is odd");   
+}
+
+if(fourthNum %2 ==0){
+    console.log("The num", fourthNum , "is even");   
+} else{
+    console.log("The num", fourthNum , "is odd");   
+}
