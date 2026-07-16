542e439471c545503ad25730819ca864b8bf1bbd
Heena <hk7780050@gmail.com>
2026-07-16 14:00:43 +0530

    script.js

--- script.js
+++ script.js
@@ -1,107 +1,21 @@
-const students = [
-    {
-        name:"Heena",
-        marks:"90%",
-        class:"10th"
-    },
-    {
-        name:"Anchal",
-        marks:"89%",
-        class:"12th"
-    },
-    {
-        name:"Riya",
-        marks:"88%",
-        class:"8th"
-    },
-    {
-        name:"Neha",
-        marks:"90%",
-        class:"12th"
-    },
-    {
-        name:"Rohan",
-        marks:"79%",
-        class:"9th"
-    },
-    {
-        name:"Aman",
-        marks:"96%",
-        class:"10th"
-    },
-    {
-        name:"Pooja",
-        marks:"69%",
-        class:"12th"
-    },
+function greetUser(){
+    const name = document.getElementById("name").value;
 
-    {
-        name:"Rohit",
-        marks:"85%",
-        class:"8th"
-    },
-    {
-        name:"Kajal",
-        marks:"78%",
-        class:"11th"
-    },
-    {
-        name:"Diya",
-        marks:"69%",
-        class:"6th"
-    },
-
-    {
-        name:"Nitin",
-        marks:"85%",
-        class:"7th"
-    },
-    {
-        name:"Shreya",
-        marks:"83%",
-        class:"11th"
-    },
-    {
-        name:"Sakshi",
-        marks:"56%",
-        class:"10th"
-    },
-
-    {
-        name:"Monika",
-        marks:"87%",
-        class:"8th"
-    },
-    {
-        name:"Harman",
-        marks:"90%",
-        class:"6th"
+    if(name===""){
+        document.getElementById("greeting").innerHTML = "Hello";
+    }else{
+        document.getElementById("greeting").innerHTML =" Hello, "+name;
     }
-];
+}
 
-const cards =document.getElementById("studentcards");
-const search =document.getElementById("search");
+function changeColor(box,color){
+    box.style.backgroundColor = color;
 
-function displayStudent(data){
-    if(data.length ===0){
-        cards.innerHTML=`<p style="grid-column":1/-1; text-align:center; color:#888;>No student found.</p>`;
-        return;
+    if(color ==="blue"){
+        box.style.color ="white";
+    }else if(color ==="green"){
+        box.style.color ="white";
+    }else{
+        box.style.color ="black";
     }
-    cards.innerHTML=data.map(student=>
-        `<div class="card">
-            <p><strong>Student Name:</strong>${student.name}</p>
-            <p><strong>Marks:</strong>${student.marks}</p>
-            <p><strong>Class:</strong>${student.class}</p>
-            </div>`
-    ).join("");
 }
-displayStudent(students);
-
-function filterStudents(){
-    const searchTerm = search.value.toLowerCase();
-
-    const filteredList = students.filter(student=> student.name.toLowerCase().includes(searchTerm));
-
-    displayStudent(filteredList);
-}
-document.getElementById("search-btn").addEventListener("click",filterStudents);
